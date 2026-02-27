3366bd6a42395754ab872e5150df33b85ae3a2f0
Oatmeal Plays <oatmealplays80@gmail.com>
2026-02-27 19:21:45 -0400

    fish (im done making commit comments on VS code)

--- news.js
+++ news.js
@@ -1,9 +1,12 @@
-// This is a "wrapper" function that sends the data to your page
-oatSS_Callback([
-  {
-    "title": "OatSS via Script Injection",
-    "date": "Feb 27, 2026",
-    "desc": "Defeated the CORS monster using a JSONP wrapper.",
-    "url": "https://github.com/oatmeal3ds"
-  }
-]);
+// news.js
+oatSS_Callback({
+  "progress": 72, // Change this number to update the bar!
+  "scrolls": [
+    {
+      "title": "OatSS Progress Engine",
+      "date": "Feb 27, 2026",
+      "desc": "The progress bar is now dynamically linked to this file.",
+      "url": "#"
+    }
+  ]
+});
