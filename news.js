8c4d6c03dec64933b4ba53c6841da25b4493c58e
Oatmeal Plays <oatmealplays80@gmail.com>
2026-02-27 19:06:00 -0400

    Update and rename news.json to news.js

---
+++ news.js
@@ -0,0 +1,9 @@
+// This is a "wrapper" function that sends the data to your page
+oatSS_Callback([
+  {
+    "title": "OatSS via Script Injection",
+    "date": "Feb 27, 2026",
+    "desc": "Defeated the CORS monster using a JSONP wrapper.",
+    "url": "https://github.com/oatmeal3ds"
+  }
+]);
