ab7703c576049a5eb13f6d1736ab41b3ea91591f
Oatmeal Plays <oatmealplays80@gmail.com>
2026-03-01 17:28:09 -0400

    Update news.js

--- news.js
+++ news.js
@@ -1,6 +1,12 @@
 // This is a "wrapper" function that sends the data to your page
 oatSS_Callback([
   {
+    "title": "New admin panel",
+    "date": "",
+    "desc": "Here on this admin panel I can make new news! need to add more features tho",
+    "url": "https://oatmeal3ds.github.io/"
+  },
+  {
     "title": "OatSS via Script Injection",
     "date": "Feb 27, 2026",
     "desc": "Defeated the CORS monster using a JSONP wrapper.",
