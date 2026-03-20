6fbc7afb8ee02b1591b74920cd56f7d05cf7b624
Oatmeal Plays <oatmealplays80@gmail.com>
2026-03-20 19:11:56 -0400

    Update news.js

--- news.js
+++ news.js
@@ -1,27 +1,35 @@
 // This is a "wrapper" function that sends the data to your page
 oatSS_Callback([
   {
-    "title": "Added a search bar to news",
-    "date": "Mar 20, 2026",
-    "desc": "Go have a look! its really cool if you wanna search old news fast",
-    "url": "https://oatmeal3ds.pages.dev/news"
+    "id": "searchbar1",
+    "t": "Added a search bar to news",
+    "dt": "2026-03-20",
+    "d": "Go have a look! its really cool if you wanna search old news fast",
+    "u": "https://oatmeal3ds.pages.dev/news",
+    "c": ["site", "feature"]
   },
   {
-    "title": "Migrated from Github Pages to Cloudflare Pages",
-    "date": "Mar 8, 2026",
-    "desc": "lol kinda ironic that most features are locked behind a damm domain",
-    "url": ""
+    "id": "cloudflare1",
+    "t": "Migrated from Github Pages to Cloudflare Pages",
+    "dt": "2026-03-08",
+    "d": "lol kinda ironic that most features are locked behind a damm domain",
+    "u": "",
+    "c": ["site"]
   },
   {
-    "title": "New admin panel",
-    "date": "Mar 1, 2026",
-    "desc": "Here on this admin panel I can make new news! need to add more features tho",
-    "url": "/"
+    "id": "admin1",
+    "t": "New admin panel",
+    "dt": "2026-03-01",
+    "d": "Here on this admin panel I can make new news! need to add more features tho",
+    "u": "/",
+    "c": ["site"]
   },
   {
-    "title": "OatSS via Script Injection",
-    "date": "Feb 27, 2026",
-    "desc": "Defeated the CORS monster using a JSONP wrapper.",
-    "url": "https://github.com/oatmeal3ds"
+    "id": "oatss1",
+    "t": "OatSS via Script Injection",
+    "dt": "2026-02-27",
+    "d": "Defeated the CORS monster using a JSONP wrapper.",
+    "u": "https://github.com/oatmeal3ds",
+    "c": ["site", "feature"]
   }
 ]);
