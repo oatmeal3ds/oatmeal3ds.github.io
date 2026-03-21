54c8143251e09c6e7ab2a5aaaeafbb086f52527f
Oatmeal Plays <oatmealplays80@gmail.com>
2026-03-20 21:47:46 -0400

    Update news.js

--- news.js
+++ news.js
@@ -1,12 +1,27 @@
 // This is a "wrapper" function that sends the data to your page
 oatSS_Callback([
   {
+    "id": "",
+    "t": "Updated how news works",
+    "dt": "2026-03-21",
+    "d": "Look at the news! Its better!",
+    "u": "https://oatmeal3ds.pages.dev/news",
+    "c": [
+      "feature",
+      "announcement"
+    ],
+    "f": "The news became better!\n\nWhat I added was:\n* Expanded news - What you are reading\n* Categories - This is why there is a banner on the homepage!\n* And more!\n\n### OatSS Has reached the 1.2 update and it has become a pretty good alternative to blogger!\nstill have to manually commit tho, might use something that cloudflare provides???"
+  },
+  {
     "id": "searchbar1",
     "t": "Added a search bar to news",
     "dt": "2026-03-20",
     "d": "Go have a look! its really cool if you wanna search old news fast",
     "u": "https://oatmeal3ds.pages.dev/news",
-    "c": ["site", "feature"]
+    "c": [
+      "site",
+      "feature"
+    ]
   },
   {
     "id": "cloudflare1",
@@ -14,7 +29,9 @@ oatSS_Callback([
     "dt": "2026-03-08",
     "d": "lol kinda ironic that most features are locked behind a damm domain",
     "u": "",
-    "c": ["site"]
+    "c": [
+      "site"
+    ]
   },
   {
     "id": "admin1",
@@ -22,7 +39,9 @@ oatSS_Callback([
     "dt": "2026-03-01",
     "d": "Here on this admin panel I can make new news! need to add more features tho",
     "u": "/",
-    "c": ["site"]
+    "c": [
+      "site"
+    ]
   },
   {
     "id": "oatss1",
@@ -30,6 +49,9 @@ oatSS_Callback([
     "dt": "2026-02-27",
     "d": "Defeated the CORS monster using a JSONP wrapper.",
     "u": "https://github.com/oatmeal3ds",
-    "c": ["site", "feature"]
+    "c": [
+      "site",
+      "feature"
+    ]
   }
 ]);
