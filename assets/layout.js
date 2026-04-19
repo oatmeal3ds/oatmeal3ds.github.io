d8652e7e91cf6d9ce4be5a9130a58905f30f2cb7
Oatmeal Plays <oatmealplays80@gmail.com>
2026-04-19 11:41:19 -0400

    Enhance footer with licensing details and icons
    Updated footer callback to include licensing information and icons.

--- assets/layout.js
+++ assets/layout.js
@@ -110,7 +110,11 @@ oatHeader_Callback(`
   </header>
 `);
 
-oatFooter_Callback(`© Oatmeal Plays — Made by Oatmeal Plays, Maintained by a bunch of mountain air and pixelated cats.`);
+oatFooter_Callback(`© Oatmeal Plays — Made by Oatmeal Plays, Maintained by a bunch of mountain air and pixelated cats. This site is licensed under 
+<a href="https://creativecommons.org/licenses/by-nc/4.0/">CC BY‑NC 4.0</a>
+<img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" style="max-width:1em;max-height:1em;margin-left:.2em;">
+<img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" style="max-width:1em;max-height:1em;margin-left:.2em;">
+<img src="https://mirrors.creativecommons.org/presskit/icons/nc.svg" style="max-width:1em;max-height:1em;margin-left:.2em;">`);
 
 function initCodeBlocks() {
   document.querySelectorAll('pre').forEach(pre => {
