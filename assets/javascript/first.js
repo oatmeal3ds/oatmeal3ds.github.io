cfa9f3aae075553145c86a763e166e5cf6d2eb6b
Oatmeal Plays <oatmealplays80@gmail.com>
2026-05-30 23:12:45 -0400

    Add riddle game to first.js

---
+++ assets/javascript/first.js
@@ -0,0 +1,11 @@
+let riddle = "I change colors and I hover like a UFO. I also have a skirt that lets me swim and various tentacles on my face. What am I?"
+let answer = "cuttlefish"
+
+console.log(riddle)
+const input = prompt("Answer: ");
+if (input.toLowerCase().includes(answer.toLowerCase())) { // what the fuck
+    console.log("You are correct!")
+} else {
+    console.log("You are incorrect!");
+}
+// JS sorcery
